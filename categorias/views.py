17d1a49be9ec17549091ffbdff1888fbc0e89c88
adolfocustodio <adolfocustodio14@gmail.com>
2023-12-19 11:25:03 -0300

    views de postagens

--- categorias/views.py
+++ categorias/views.py
@@ -1,1 +1,25 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Categoria
+from .forms import CategoriaForm
+from django.urls import reverse_lazy
+
+
+class CategoriaCriar(generic.CreateView):
+    form_class = CategoriaForm
+    model = Categoria
+    success_url = reverse_lazy('categorias:categoria_listar')
+
+
+class CategoriaListar(generic.ListView):
+    model = Categoria
+
+
+class CategoriaEditar(generic.UpdateView):
+    form_class = CategoriaForm
+    model = Categoria
+    success_url = reverse_lazy('core:categoria_listar')
+
+
+class CategoriaExcluir(generic.DeleteView):
+    model = Categoria
+    success_url = reverse_lazy('categorias:categoria_listar')
--- crono/urls.py
+++ crono/urls.py
@@ -4,5 +4,7 @@ from django.contrib import admin
 
 urlpatterns = [
     path('', include('core.urls', namespace='core')),
+    path('categorias', include('categorias.urls', namespace='categorias')),
+    path('postagens', include('postagens.urls', namespace='postagens')),
     path('admin/', admin.site.urls)
 ]
