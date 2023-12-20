9e111ac98fa49873ff09eeced3a8e6ef5f340a50
adolfocustodio <adolfocustodio14@gmail.com>
2023-12-19 23:01:15 -0300

    layout das postagens

--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
+from postagens.models import Postagem
 
 
 class Inicio(TemplateView):
     template_name = 'core/inicio.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['postagens'] = Postagem.objects.all()
+        return context
